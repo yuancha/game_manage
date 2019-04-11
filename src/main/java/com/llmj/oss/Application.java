0d2479e1be7a3ce0cc9f7ddb2d7c67c70186c5fd
yuanchao <yuancjava@163.com>
2019-04-11 21:28:42 +0800

    1

--- src/main/java/com/llmj/oss/Application.java
+++ src/main/java/com/llmj/oss/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		log.info("Oss_server start ----->>>>>>>>>>>>>>>>>>>>>>>>> ");
+		log.info("Oss_server start ----->>>>>>>>>>>1>>>>>>>>>>>>>> ");
 	}
 	
 }
